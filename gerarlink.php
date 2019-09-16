7a5ee79cfbbeb6113971eafcfede42b2ad034422
Joel Silva <53149347+zoreu@users.noreply.github.com>
2019-09-16 16:16:31 -0300

    Update gerarlink.php

--- gerarlink.php
+++ gerarlink.php
@@ -1,14 +1,14 @@
 <?php
-$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-$url = $_GET["url"]
-//$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+//$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+$url = $_GET["url"];
+$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
 $ch = curl_init();
 $timeout = 5;
 // curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_VERBOSE, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 // curl_setopt($ch, CURLOPT_HEADER, TRUE);
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
